cad1bb3a24074e1fbd3b1e896f91cbf864e9d18a
ramirocontreras5 <ramirocontreraslb@gmail.com>
2026-09-25 19:56:37 -0300

    Initial commit: cron log analyzer

---
+++ analyzer.py
@@ -0,0 +1,16 @@
+with open("cron_log.txt") as cron_file:
+    lines = cron_file.readlines()
+
+commands_counter= 0
+
+
+for line in lines:
+    
+    if "CMD" in line:
+        parts = line.strip().rsplit("CMD (", 1)
+        timestamp = parts[0].split(" ")
+        time = timestamp[2]
+        print(time, parts[1].strip(")"))
+        commands_counter += 1
+
+print(f"Total commands executed: {commands_counter}")
